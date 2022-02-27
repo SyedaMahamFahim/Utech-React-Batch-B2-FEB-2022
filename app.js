1ae381dd2a27433c845932d3beda358a17fd504c
SyedaMahamFahim <fahim4303107@cloud.neduet.edu.pk>
2022-02-27 22:58:18 +0500

    commit

---
+++ app.js
@@ -0,0 +1,58 @@
+// String
+// var userName="Maham";
+// console.log(userName)
+
+// Number
+// var x=9
+// var y=9
+// console.log(x+y) 
+
+// Array 
+// var studentsName=["ayesha", "ameen"];
+// console.log(studentsName[0])
+
+// Object
+// var ayeshaDetail={
+//     fullname:"Ayesha Khan",
+//     grade:7,
+// }
+
+// console.log("My name is "+ ayeshaDetail.fullname)
+
+// Array Of Object
+// var studentsDetails=[
+//     {
+//         id:1,
+//         fullname:"Ayesha Khan",
+//         grade:7,
+//     },
+//     {
+//         id:2,
+//         fullname:"Muhammad Ameen",
+//         grade:6,
+//     }
+// ];
+// console.log(studentsDetails[0].fullname)
+
+//Conditional Statement 
+// >=   greater and equal to
+// <= less than and equal to 
+// > greater
+// < less 
+
+
+var x=21
+if(x >= 21){
+    console.log("x is greater than 21")
+}else{
+    console.log("no")
+}
+
+
+//String
+//Number
+//Array
+//Object
+//undefined
+//Boolean
+//Null
